7a6eab962faa998f686b62c6ff1bc968c4485e89
michal <Michalelikishvili@gmail.com>
2022-02-09 18:50:53 +0200

    Added is_premuim attribute to class user

--- core/users/user.py
+++ core/users/user.py
@@ -1,5 +1,5 @@
 class User:
-  def __init__(self, id, name, playlists):
+  def __init__(self, id, name, playlists, is_premuim):
     self.id = id
     self.name = name
     self.playlists = playlists
