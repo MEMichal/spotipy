0b1a95bf7ddf81a723abfde62e11d4d979433b97
michal <Michalelikishvili@gmail.com>
2022-02-10 21:15:38 +0200

    added imports to access objects

--- core/reading_loading/__init__.py
+++ core/reading_loading/__init__.py
@@ -1,1 +1,4 @@
 from core.reading_loading.load_songs import load_songs
+from core.reading_loading.load_songs import artists
+from core.reading_loading.load_songs import albums
+from core.reading_loading.load_songs import songs
