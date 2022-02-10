3d7afcb2c17cf0de6e401e170c8120a22b421f02
michal <Michalelikishvili@gmail.com>
2022-02-10 09:26:02 +0200

    modified classes to impelment songs_ids and playlists_ids and not songs and playists

--- core/users/playlist.py
+++ core/users/playlist.py
@@ -1,5 +1,5 @@
 class Playlist:
-  def __init__(self, id, name, songs):
+  def __init__(self, id, name, songs_ids):
     self.id = id
     self.name = name
-    self.songs = songs
+    self.songs_ids = songs_ids
--- core/users/user.py
+++ core/users/user.py
@@ -1,5 +1,5 @@
 class User:
-  def __init__(self, id, name, playlists, is_premuim):
+  def __init__(self, id, name, playlists_ids, is_premuim):
     self.id = id
     self.name = name
-    self.playlists = playlists
+    self.playlists_ids = playlists_ids
